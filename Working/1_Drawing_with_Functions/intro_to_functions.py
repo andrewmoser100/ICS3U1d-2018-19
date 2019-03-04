91aca47877f336712bc632c62566da8aa82d69bd
robuntu <robuntu@stro.ycdsb.ca>
2019-03-04 11:10:27 -0500

    commit

---
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -0,0 +1,47 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# define your draw functions
+
+def draw_cloud():
+    """ Draw a cloud """
+    arcade.draw_circle_filled(300, 500, 60, arcade.color.WHITE)
+    arcade.draw_circle_filled(300, 580, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(260, 540, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(360, 540, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(360, 520, 40, arcade.color.WHITE)
+
+    arcade.draw_circle_filled(660, 540, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(630, 540, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(660, 490, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(620, 510, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(670, 500, 40, arcade.color.WHITE)
+
+def draw_rollinghills():
+    arcade.draw_circle_filled(50, 80, 300, arcade.color.GREEN)
+    arcade.draw_circle_filled(200, 80, 300, arcade.color.GREEN)
+    arcade.draw_circle_filled(700, 80, 300, arcade.color.GREEN)
+
+def draw_tree():
+    arcade.draw_circle_filled(200, 200, 100, arcade.color.DARK_GREEN)
+    arcade.draw_rectangle_filled(50, 10, 300, 300, arcade.color.BROWN)
+
+def main():
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
+    arcade.set_background_color(arcade.color.DARK_BLUE)
+    arcade.start_render()
+
+   # call your draw functions
+    draw_cloud()
+    draw_rollinghills()
+    draw_tree()
+
+    # Finish and run
+    arcade.finish_render()
+    arcade.run()
+
+
+# Call the main function to get the program started.
+main()
